6cf639c0a2f0c95b5f7d8a1533f3476a78eca5d2
Lucas Postiglioni Obes <73368@PROWAY.TREINA>
2023-07-15 18:08:00 -0300

    Introduzido o conteúdo de ASP.NET Core com exemplos utilizando HTML e Java Script, e exercícios para fixação de conteúdo

---
+++ LojaWeb/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using LojaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LojaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ExemploCampos()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroPersonagem()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarPersonagem([FromQuery] string nome, [FromQuery] int idade)
+        {
+            var anohoje = DateTime.Now.Year;
+            var anoNascimento = anohoje - idade;
+            var status = "";
+            if (idade < 18)
+            {
+                status = "Menor de 18 anos";
+            }
+            else
+            {
+                status = "Maior ou igual a 18 anos";
+            }
+
+            var mensagem = $"{nome} nasceu em {anoNascimento}. Status: {status}";
+
+            return Ok(mensagem);
+        }
+
+        public IActionResult CadastroNome()
+        {
+            return View();
+        }
+
+        public IActionResult NomeCompleto([FromQuery] string nome, [FromQuery] string sobrenome)
+        {
+            var mensagem = $"O seu nome completo é: {nome} {sobrenome}";
+
+            return Ok(mensagem);
+        }
+
+        public IActionResult DadosImc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalcularImc([FromForm] string nome, [FromForm] double altura, [FromForm] double peso)
+        {
+            var calculoImc = peso / Math.Pow(altura, 2);
+
+            var mensagem = $"Olá {nome}, o seu IMC é de {calculoImc}";
+            return Ok(mensagem);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
